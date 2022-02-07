46f829e571ff71655171824b87b7b94487124cd3
Serhii_Cheremisin <s.cheremisin1991@gmail.com>
2022-02-07 09:48:28 +0200

    added docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,33 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ServicesModule } from './services/services.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import {User} from "./users/entities/user.entity";
-import {Service} from "./services/entities/service.entity";
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UsersModule} from './users/users.module';
+import {ServicesModule} from './services/services.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
-  imports: [UsersModule, ServicesModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'chs',
-    password: 'qwerty',
-    database: 'chs',
-    entities: [User, Service],
-    synchronize: true,
-  })],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({isGlobal: true, envFilePath: '.env'}),
+        UsersModule,
+        ServicesModule,
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (config: ConfigService) => ({
+                type: config.get<'aurora-data-api'>('TYPEORM_CONNECTION'),
+                username: config.get<string>('TYPEORM_USERNAME'),
+                password: config.get<string>('TYPEORM_PASSWORD'),
+                database: config.get<string>('TYPEORM_DATABASE'),
+                port: config.get<number>('TYPEORM_PORT'),
+                entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+                synchronize: true,
+                autoLoadEntities: true,
+                logging: true
+            })
+        })],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
